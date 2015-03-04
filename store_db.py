b1aee9190c58656090dc4229265cfcc7f136d004
ricky kwok <ricky9487@gmail.com>
2015-03-03 17:20:40 -0800

    organize create and executes

--- store_db.py
+++ store_db.py
@@ -18,45 +18,66 @@ def make_table(reviews):
 	conn = lite.connect('hotels.db')
 	cur = conn.cursor()
 
-	# drop table if it exists
+	# drop tables if exists for re-inserting
+	cur.execute('DROP TABLE IF EXISTS Hotel_info')
+	cur.execute('DROP TABLE IF EXISTS Review')
+	cur.execute('DROP TABLE IF EXISTS Rating')
 	cur.execute('DROP TABLE IF EXISTS Aspect')
 
 	# create table with the nonempty data in <filename>
-	cur.execute('''CREATE TABLE Review 
-				   (Author TEXT,
-				    Content VARCHAR(MAX),
-				    Occurrence TEXT,
-				    Rating  TEXT
-				    Aspect0 VARCHAR(MAX),
-				    Aspect1 VARCHAR(MAX),
-				    Aspect2 VARCHAR(MAX),
-				    Aspect3 VARCHAR(MAX),
-				    Aspect4 VARCHAR(MAX),
-				    Aspect5 VARCHAR(MAX),
-				    Aspect6 VARCHAR(MAX),
-				    Aspect7 VARCHAR(MAX),
+	cur.execute('''CREATE TABLE Key
+				   (Entry_id INT,
+				    Hotel_id INT,
+				    Author_id TEXT,
 				    ) ''')
+
+	cur.execute('''CREATE TABLE Hotel_info
+				   (Hotel_id INT,
+				    Price INT,
+				    Location TEXT,
+				    ) ''')
+
+	cur.execute('''CREATE TABLE Review
+				   (Entry_id INT,
+				    Review_date TEXT,
+				    Content VARCHAR(MAX)
+				    ) ''')
+
+
+	cur.execute('''CREATE TABLE Rating 
+				   (Entry_id TEXT,
+					Overall INT,
+					Value INT,
+					Room INT,
+					Location INT,
+					Cleanliness INT,
+					Checkin INT,
+					Service INT,
+					BusinessService INT
+					)''')
+
+	cur.execute('''CREATE TABLE Aspect
+				   (Entry_id TEXT,
+					Aspect1 TEXT,
+					Aspect2 TEXT,
+					Aspect3 TEXT,
+					Aspect4 TEXT,
+					Aspect5 TEXT,
+					Aspect6 TEXT,
+					Aspect7 TEXT,
+					Weight1 INT,
+					Weight2 INT,
+					Weight3 INT,
+					Weight4 INT,
+					Weight5 INT,
+					Weight6 INT,
+					Weight7 INT
+					)''')
+
 	cur.execute(''' INSERT INTO Review VALUES 
 
-		''')
+						) ''')
 
 def get_review(filename):
 	text = read_one_aspect(filename)
 	return text 
-
-
-"""
-cur.execute('''CREATE TABLE Aspect 
-			   ('Author' TEXT,
-				'Content' TEXT,
-				'Date' TEXT
-				'OverallRating' INT,
-				'Value' INT,
-				'Room' INT,
-				'Location' INT,
-				'Cleanliness' INT,
-				'Checkin' INT,
-				'Service' INT,
-				'BusinessService' INT,
-				'Aspect'	)''')
-"""
